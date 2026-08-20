d7688fe406a28e07c567f10c25321165858d4712
Neamow <neamow@gmail.com>
2026-08-20 13:57:35 +0200

    Initial commit

---
+++ src/main/java/net/neamow/blockvariantswapper/BlockVariantSwapperClient.java
@@ -0,0 +1,11 @@
+package net.neamow.blockvariantswapper;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class BlockVariantSwapperClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
