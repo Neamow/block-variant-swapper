c3ca78aa1c955eabef9ae28369d40b692b09f46c
Neamow <neamow@gmail.com>
2026-08-23 16:01:49 +0200

    Deeper and Darker compat

--- src/main/java/net/neamow/blockvariantswapper/BlockVariantConfig.java
+++ src/main/java/net/neamow/blockvariantswapper/BlockVariantConfig.java
@@ -32,7 +32,8 @@ public class BlockVariantConfig {
         "/assets/" + BlockVariantSwapper.MOD_ID + "/config/biomeswevegone_block_variants.json",
         "/assets/" + BlockVariantSwapper.MOD_ID + "/config/betternether_block_variants.json",
         "/assets/" + BlockVariantSwapper.MOD_ID + "/config/betterend_block_variants.json",
-        "/assets/" + BlockVariantSwapper.MOD_ID + "/config/twilightforest_block_variants.json"
+        "/assets/" + BlockVariantSwapper.MOD_ID + "/config/twilightforest_block_variants.json",
+        "/assets/" + BlockVariantSwapper.MOD_ID + "/config/deeperdarker_block_variants.json"
     );
 
     private static Map<String, List<String>> mergedVariants = new LinkedHashMap<>();
