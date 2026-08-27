94e9aca0d57ed332c077777027b58a9ade877949
Neamow <neamow@gmail.com>
2026-08-27 19:12:16 +0200

    Ecologics compat

--- src/main/java/net/neamow/blockvariantswapper/BlockVariantConfig.java
+++ src/main/java/net/neamow/blockvariantswapper/BlockVariantConfig.java
@@ -30,7 +30,8 @@ public class BlockVariantConfig {
         "/assets/" + BlockVariantSwapper.MOD_ID + "/config/minecraft_block_variants.json",
         "/assets/" + BlockVariantSwapper.MOD_ID + "/config/biomesoplenty_block_variants.json",
         "/assets/" + BlockVariantSwapper.MOD_ID + "/config/betterend_block_variants.json",
-        "/assets/" + BlockVariantSwapper.MOD_ID + "/config/betternether_block_variants.json"
+        "/assets/" + BlockVariantSwapper.MOD_ID + "/config/betternether_block_variants.json",
+        "/assets/" + BlockVariantSwapper.MOD_ID + "/config/ecologics_block_variants.json"
     );
 
     private static Map<String, List<String>> mergedVariants = new LinkedHashMap<>();
